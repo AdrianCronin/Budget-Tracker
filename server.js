e1012afa7b9afebc77e36135d3b7004ae1737dfd
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-09-20 16:53:09 -0700

    added a routes variable and put the require of api.js into it. changed app.use to call the routes variable

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+const routes = require("./routes/api.js");
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -21,7 +23,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
