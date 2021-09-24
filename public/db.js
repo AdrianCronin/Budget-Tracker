8d0bf835857e5111d1fbdc2296fa1de16764cfbc
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-09-23 20:56:39 -0700

    created the checkDatabase function

--- public/db.js
+++ public/db.js
@@ -17,7 +17,7 @@ request.onsuccess = event => {
 request.onupgradeneeded = event => {
     db = event.target.result;
 
-    db.createObjectStore("budgetIndexDB", {
+    db.createObjectStore("budgetStore", {
         keyPath: "transactionID",
         autoIncrement: true
     });
@@ -26,8 +26,38 @@ request.onupgradeneeded = event => {
 
 
 const checkDatabase = () => {
+    let transaction = db.transaction(["budgetStore"], "readwrite");
 
+    const store = transaction.objectStore("budgetStore");
 
+    const getStore = store.getAll();
+
+    getStore.onsuccess = async () => {
+        try {
+            if (getStore.result.length > 0) {
+
+                const response = await fetch('/api/transaction/bulk', {
+                    method: 'POST',
+                    body: JSON.stringify(getAll.result),
+                    headers: {
+                        Accept: 'application/json, text/plain, */*',
+                        'Content-Type': 'application/json',
+                    },
+                });
+
+                const data = await response.json();
+
+                if (data.length !== 0) {
+                    transaction = db.transaction(["budgetStore"], "readwrite");
+
+                    const newStore = transaction.objectStore("budgetStore");
+                    newStore.clear();
+                }
+            }
+        } catch (err) {
+            console.error(err);
+        }
+    }
 };
 
 const saveRecord = () => {
